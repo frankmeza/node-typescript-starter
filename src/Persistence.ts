062b4fcd403bd42b13cc67b32d77a8bb9e6d8cba
Frank Meza <frankmezapdx@gmail.com>
2017-10-04 11:17:16 -0700

    adds new person into items object, and sorts within sort array

---
+++ src/Persistence.ts
@@ -0,0 +1,36 @@
+class Persistence {
+  people
+  pets
+
+  constructor() {
+    this.getPeople = this.getPeople.bind(this)
+    this.addPerson = this.addPerson.bind(this)
+
+    this.people = {
+      items: {},
+      sort: [],
+    }
+    this.pets = {
+      items: {},
+      sort: [],
+    }
+  }
+
+  getPeople() {
+    return this.people
+  }
+
+  addPerson(person) {
+    const people = this.people
+    const peopleObject = { ...people }
+
+    peopleObject.items[person.id] = person
+    peopleObject.sort.push(person)
+    peopleObject.sort.sort((a, b) => a.id > b.id)
+    
+    return peopleObject
+  }
+
+}
+
+export default Persistence
