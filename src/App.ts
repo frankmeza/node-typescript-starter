4bb2d666c0e09c544556237bcc594867db68dcbe
Frank Meza <frankmezapdx@gmail.com>
2017-10-06 14:53:00 -0700

    add more type checking

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,7 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
 import Persistence from './Persistence'
+import { Person, Pet } from './Persistence'
 
 class App {
   public express: express
@@ -18,39 +19,40 @@ class App {
     const router = express.Router()
 
     router.get('/', (req: express.Request, res: express.Response) => {
-      res.json({true: true})
+      res.json({ true: true })
     })
 
     // PEOPLE
     // index people
     router.get('/persons', (req: express.Request, res: express.Response) => {
-      const people: People = this.persistence.getPeople()
+      const people: Person[] = this.persistence.getPeople()
       res.json(people)
     })
 
     // show person
     router.get('/persons/:id', (req: express.Request, res: express.Response) => {
       const id: string = req.params.id
-      res.json(this.persistence.getPerson(id))
+      const person: Person = this.persistence.getPerson(id)
+      res.json(person)
     })
 
     // create person
     router.post('/persons', (req: express.Request, res: express.Response) => {
-      const person = req.body
-      const people = this.persistence.addPerson(person)
+      const person: Person = req.body
+      this.persistence.addPerson(person)
       res.sendStatus(201)
     })
     
     // update person
     router.put('/persons', (req: express.Request, res: express.Response) => {
-      const person = req.body
+      const person: Person = req.body
       this.persistence.updatePerson(person)
       res.sendStatus(204)
     })
     
     // delete person
     router.delete('/persons/:id', (req: express.Request, res: express.Response) => {
-      const id = req.params.id
+      const id: string = req.params.id
       this.persistence.deletePerson(id)
       res.sendStatus(204)
     })
